1831e41f7836f6a24f5e87726848c377e6bbe77f
RamVignesh B <ramvigneshgreat@pec.edu>
2020-05-10 17:58:00 +0530

    Rebuilding the archive

--- www/server.js
+++ www/server.js
@@ -11,13 +11,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const util_1 = require("./util/util");
 const fs_1 = __importDefault(require("fs"));
 (() => __awaiter(this, void 0, void 0, function* () {
     // Init the Express application
-    const app = express_1.default();
+    const express = require('express');
+    const app = express();
     // Set the network port
     const port = process.env.PORT || 8082;
     // Use the body parser middleware for post requests
@@ -36,8 +36,9 @@ const fs_1 = __importDefault(require("fs"));
     // RETURNS
     //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
     /**************************************************************************** */
+    let image_url;
     app.get("/filteredimage", (req, res) => __awaiter(this, void 0, void 0, function* () {
-        let { image_url } = req.query;
+        image_url = req.query;
         yield util_1.filterImageFromURL(image_url).then((image) => {
             fs_1.default.readFile(image, (err, data) => {
                 if (err) {
