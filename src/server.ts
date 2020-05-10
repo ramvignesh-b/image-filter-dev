fdec4f73fd6b2cd4f20d36088beb8824b5e54d42
RamVignesh B <ramvigneshgreat@pec.edu>
2020-05-10 14:11:37 +0530

    hard typer the req, res and image_url variables

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,9 @@ import fs from 'fs';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
+  let image_url: string;
+  let req: Request;
+  let res: Response;
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
